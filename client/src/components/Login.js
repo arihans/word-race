bee2cbf2e8c61b106b6baa38ce90ac871747a92d
Arindam <arindamhansda484@gmail.com>
2023-01-19 23:24:48 +0530

    Add screenshot

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
+import '../styles/Login.css';
+
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [errorMsg, setErrorMsg] = useState('')
+    const [errorMsg, setErrorMsg] = useState('');
     const [showError, setShowError] = useState(false);
 
     const navigate = useNavigate();
@@ -22,7 +24,6 @@ const Login = () => {
                     setErrorMsg(response.data.error);
                 } else {
                     navigate('/game');
-                    // TODO: Handle successful registration
                 }
             })
             .catch((error) => {
@@ -34,36 +35,40 @@ const Login = () => {
 
     useEffect(() => {
         if (showError) {
-            setTimeout(() => {
-                setShowError(false);
-            }, 3000);
+            setTimeout(() =>
+                setShowError(false)
+            , 10000);
+        } else {
+            setShowError(false);
         }
+
+        return () => clearTimeout(setTimeout);
     }, [showError]);
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
-            </label>
-            <br />
-            <label>
-                Password:
-                <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-            <br />
-            <button type="button" onClick={() => navigate('/register')}>
-                New user? Click here to register
-            </button>
-
+        <div className='Login'>
+            <form onSubmit={handleSubmit}>
+                <div className='input-container'>
+                    <input type='text' value={username} placeholder='User Name'
+                        onChange={(event) => setUsername(event.target.value)} />
+                </div>
+                <div className='input-container'>
+                    <input type='password' value={password} placeholder='Password'
+                        onChange={(event) => setUsername(event.target.value)} />
+                </div>
+                <div className='button-container'>
+                    <button type='submit'>Submit</button>
+                    <button type='button' className='register-button' onClick={() => navigate('/register')}>
+                        New user? Click here to register
+                    </button>
+                </div>
+            </form>
             {showError && (
-                <div className="error-message">
+                <div className='error-message'>
                     {errorMsg}
                 </div>
             )}
-        </form>
+        </div>
     );
 }
 
